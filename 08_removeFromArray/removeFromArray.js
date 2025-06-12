dfa5f29454d9b816c9a959c8ed4bd74331a59c33
Sofiane Chatelet <184280384+PepsPrime@users.noreply.github.com.>
2025-06-12 22:31:02 +0200

    finsihed exercise 06 to 09

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...args) {
+    for (let arg of args) {
+        while (arr.includes(arg)) {
+            let indexToDelete = arr.findIndex((element) => element == arg)
+            arr.splice(indexToDelete,1)
+            
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(min,max) {
+    let sum = 0;
+    if (max < 0 || min < 0) {
+        return 'ERROR'
+    }
+    if (!Number.isInteger(max)|| !Number.isInteger(min)) return "ERROR"
+    if (max < min) {
+        let temp = max;
+        max = min;
+        min = temp;
+    }
+    for(let i = min; i <=max;i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
